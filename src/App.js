254cafea735080b933f835a56252ce94f01c45df
co0ki3 <ehrrhgus0324@gmail.com>
2021-09-23 14:49:10 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMediaQuery } from "react-responsive";
+import { Menu } from "./menu.jsx";
+import { Button } from "./button";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 500px;
+  height: 500px;
+`;
 
 function App() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const isTablet = useMediaQuery({
+    query: "(max-width:1023px) and (min-width:768px)",
+  });
+
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
+
+  const onClick = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
+  const handleTouchEvent = (e) => {
+    e.preventDefault();
+    setIsMenuVisible(!isMenuVisible);
+  }
+
+  useEffect(() => {
+    console.log("Tablet " + isTablet);
+    console.log("Mobile " + isMobile);
+  }, [isMobile, isTablet]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isMobile || isTablet ? (
+        <Wrapper onTouchStart={handleTouchEvent}>{isMenuVisible ? <Menu /> : <></>}</Wrapper>
+      ) : (
+        <Wrapper>
+          <Button onClick={onClick}>Click Me</Button>
+          {isMenuVisible ? <Menu /> : <></>}
+        </Wrapper>
+      )}
+    </>
   );
 }
 
---
+++ src/menu.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Menu = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: black;
+  color: white;
+`;
